13830f820a4ff0b13bec33d78aa7f3de99d32bca
Gary Ng Kwong Sang <gng4820@gmail.com>
2024-12-20 11:07:02 -0700

    Add dotenv for environment variable management and update API URL in llmService

--- src/services/llmService.tsx
+++ src/services/llmService.tsx
@@ -1,12 +1,26 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000'
+const API_URL = "https://api.groq.com/openai/v1";
+const token = process.env.REACT_APP_API_KEY;
+
+const headers = {
+    'Authorization': `Bearer ${token}`
+}
 
 export const getAvailableModels = async () => {
-    return await axios.get(`${API_URL}/models`)
+    return await axios.get(`${API_URL}/models`, { headers })
 }
 
 export const sendMessagetoLLM = async (model: string, prompt: string) => {
-    const response = await axios.post(`${API_URL}/llm`, { model, prompt })
+    const requestBody = {
+      model: model,
+      messages: [
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+    };
+    const response = await axios.post(`${API_URL}/chat/completions`, requestBody, { headers })
     return response.data["choices"][0]["message"]["content"]
 }
